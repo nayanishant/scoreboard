da049a5f94eb119681ebf9d7c3de89b9377b9f0a
nyanishant <nayanishant.info@gmail.com>
2021-10-03 22:52:35 +0530

    updated name

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 
 const Button = () => {
 
-    const [count, setCount] = useState(0)
+    const [score, setScore] = useState(0)
 
     return (
 
         <div className="card-btn">
-            <p className="count">{count}</p>
-            <button className="btn-plus" onClick={() => setCount(count + 1)}>+</button>
+            <p className="count">{score}</p>
+            <button className="btn-plus" onClick={() => setScore(score + 1)}>+</button>
         </div>
     )
 }
